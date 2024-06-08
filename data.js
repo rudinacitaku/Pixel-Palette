41a1be29cfa628e22c91ab54a6fd106817d048c3
rudinacitaku <rudinacitaku@outlook.com>
2024-06-08 23:59:06 +0200

    Set up Javascript basic functions for the Categories Page

---
+++ data.js
@@ -0,0 +1,75 @@
+export const mapClasses = [ 
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+    "variant-2",
+    "variant-3",
+    "variant-1",
+];
+
+export const previews=[
+    {
+        img:'./images/newspaper.jpg',
+        title:"News",
+        tags:"Latest news",
+        description: "Exploring the latest news around the world"
+    },
+    {
+        img:'./images/makeup.jpg',
+        title:"Beauty",
+        tags:"Beauty, Skincare, Makeup",
+        description: "Exploring the latest beauty trends around the world"
+    },
+    {
+        img:'./images/fashion.jpg',
+        title:"Fashion",
+        tags:"Fashion trends, Style Guides, Latest Collections",
+        description: "Exploring the latest news around the world"
+    },
+    {
+        img:'./images/entertainment.jpg',
+        title:"Entertainment",
+        tags:"Movies, TV Shows, Music, Celebrities",
+        description: "Exploring the latest celebrity news around the world"
+    },
+    {
+        img:'./images/culture.jpg',
+        title:"Culture",
+        tags:"Art&Design, Books, History Society ",
+        description: "Exploring culture around the world"
+    },
+    {
+        img:'./images/lifestyle.jpg',
+        title:"Lifestyle",
+        tags:"Travel, Food & Drink, Home & Garden, Personal Finance",
+        description: "Exploring lifestyle"
+    },
+    {
+        img:'./images/lifestyle.jpg',
+        title:"Lifestyle",
+        tags:"Travel, Food & Drink, Home & Garden, Personal Finance",
+        description: "Exploring lifestyle"
+    },
+    {
+        img:'./images/lifestyle.jpg',
+        title:"Lifestyle",
+        tags:"Travel, Food & Drink, Home & Garden, Personal Finance",
+        description: "Exploring lifestyle"
+    },
+    {
+        img:'./images/lifestyle.jpg',
+        title:"Lifestyle",
+        tags:"Travel, Food & Drink, Home & Garden, Personal Finance",
+        description: "Exploring lifestyle"
+    },
+    {
+        img:'./images/lifestyle.jpg',
+        title:"Lifestyle",
+        tags:"Travel, Food & Drink, Home & Garden, Personal Finance",
+        description: "Exploring lifestyle"
+    },
+]
